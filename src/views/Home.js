82d7bcc5bda67984748308d0f1eb53efb775b664
maui <maui@devf.mx>
2020-05-14 20:10:16 -0500

    Agregar vistas de home y login (moldes)

---
+++ src/views/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <h1>Hola desde App</h1>
+      <p>lorem ipsum dolor sit amet</p>  
+    </React.Fragment>
+  );
+}
+ 
+export default Home;
---
+++ src/views/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <h1>Hola desde Login</h1>
+  );
+}
+ 
+export default Login;
