2d5019e3289f26a5dd34df79d65935f88f03caa0
maui <maui@devf.mx>
2020-05-14 21:15:09 -0500

    Agregar barra de navegacion usando enrutamiento

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,40 @@
- import React from 'react';
+ import React, { useState } from 'react';
+ import { Link } from 'react-router-dom';
+ import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
- const Navigation = () => {
-   return ( 
-      <h2>Navbar</h2>
-    );
- }
-  
- export default Navigation;
+
+const Navigation = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar
+      className="navbar navbar-dark bg-dark"
+      // style={{ backgroundColor: "black", color: "red" }}
+      expand="md">
+      <NavbarBrand tag={Link} to="/">Maui App</NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink tag={Link} to="/login">Login</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to="/signup">Signup</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+}
+
+export default Navigation;
