00d821fc3c3ebe9aa8e58d19cecfe0e82472b8f1
maui <maui@devf.mx>
2020-05-15 21:42:28 -0500

    Agregar maquetado de signup

--- src/views/Signup.js
+++ src/views/Signup.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import {
+  Button,  
+  Form,  
+  FormGroup,
+  Label,  
+  Input,
+} from 'reactstrap';
 
 const Signup = () => {
-  return ( 
+  return (
     <React.Fragment>
-      <h2>Signup</h2>
+      <h1 className="mb-4">Signup on Maui App</h1>
+      <Form>
+        <FormGroup>
+          <Label>First Name</Label>
+          <Input type="text" id="firstName" name="inputFirstName" 
+          placeholder="type your first name" />
+        </FormGroup>
+        <FormGroup>
+          <Label>Last Name</Label>
+          <Input type="text" name="lastName" id="inputLastname" placeholder="type your last name" />
+        </FormGroup>
+        <FormGroup>
+          <Label>Email</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="type your email" />
+        </FormGroup>
+        <FormGroup>
+          <Label>Password</Label>
+          <Input type="password" name="password" id="examplePassword" placeholder="type your password here" />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
     </React.Fragment>
   );
 }
