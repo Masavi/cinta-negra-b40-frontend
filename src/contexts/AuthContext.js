daf26c47296b3590d768ea659382ef9ed75cd734
maui <maui@devf.mx>
2020-05-19 21:25:12 -0500

    Agregar manejo de estado desde contexto

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,10 +1,28 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const AuthContext = createContext();
 
 const AuthContextProvider = (props) => {
+  const [isAuth, setAuth] = useState(false);
+
+  const setToken = (token) => {
+    localStorage.setItem('token', token);
+    setAuth(true);
+  }
+
+  const removeToken = () => {
+    localStorage.removeItem('token');
+    setAuth(false);
+  }
+
+  const getToken = () => {
+    return localStorage.getItem('token');
+  }
+
   return (
-    <AuthContext.Provider value={{ color:"blue" }}>
+    <AuthContext.Provider value={{ 
+      color:"blue", isAuth, getToken, setToken, removeToken
+    }}>
       { props.children }
     </AuthContext.Provider>
   )
